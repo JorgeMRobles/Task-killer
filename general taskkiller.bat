7bdef4ee5c579a00f6f6d86779c043ac19f8d554
JorgeMRobles <70479625+JorgeMRobles@users.noreply.github.com>
2021-07-23 14:12:20 +0200

    Add files via upload

---
+++ general taskkiller.bat
@@ -0,0 +1,9 @@
+@echo off
+taskkill /F /IM explorer.exe /IM notepad++.exe /IM msedge.exe 
+taskkill /F /IM excel.exe /IM chrome.exe /IM iexplore.exe
+taskkill /F /IM Word.exe /IM access.exe /IM notepad.exe /IM outlook.exe
+taskkill /F /IM spotify.exe /IM xampp.exe /IM draw.io.exe
+taskkill /F /IM outlook.exe /IM hamachi-2-ui.exe /IM xampp-control.exe /IM python.exe
+taskkill /F /IM eclipse.exe /IM powerpoint.exe
+taskkill /F /IM studio.exe /IM studio64.exe /IM VirtualBox.exe /IM VirtualBoxVM.exe
+Start explorer.exe 
